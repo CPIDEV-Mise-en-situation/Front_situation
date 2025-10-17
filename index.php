3439a5740d1afb049e72d4b90d0bc2a04e5d4700
Elodie PARISOT <elodie.parisot@mewo-campus.fr>
2025-10-17 11:22:35 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <img src="./img/logo.png" alt="icon"> 
+            <nav id="navigation">  
+                    <div class="nav">
+                        <ul>
+                            <li><a href="">Produit</a></li>
+                            <li><a href="">Profil</a></li>
+                            <li><a href="">Connexion</a></li>
+                        </ul>
+                    </div>     
+            </nav>
+    </header>
+</body>
+</html>
