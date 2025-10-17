20e7c6ccf471798e1ebfbe344ddd442111e8cdd8
Elodieparisot54 <elodie.parisot54700@gmail.com>
2025-10-17 15:44:17 +0200

    création pages connexion et inscription plus le css

---
+++ connexion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Connexion</title>
+        <link rel="stylesheet" href="CSS/connexion_inscription.css">
+    </head>
+    <body>
+        <div class="head">
+            <img src="IMG/drapeau_de_lorraine.png" alt="Drapeau de la Lorraine" id="logoLorraine"/>
+            <div id="headLogo">
+                <img src="IMG/logo_republique_francaise.png" alt="Logo de la république"/>
+                <img src="IMG/logo.png" alt="Logo du site" id="logo"/>
+            </div>
+            <h2>Bienvenue à la mairie de <br/>Pont-à-Mousson !</h2>
+        </div>
+
+        <div class="form">
+            <h1>Connexion: </h1>
+            <form action="">
+                <label for="mail">Mail</label>
+                <input type="email" name="mail" id="mail" class="textBox"/>
+                <label for="mdp">Mot de passe</label>
+                <input type="password" name="mdp" id="mdp" class="textBox"/>
+            </form>
+        </div>
+        <div class="buttonDiv">
+            <input type="button" value="Connexion" class="button"/>
+        </div>
+    </body>
+</html>
