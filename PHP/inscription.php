3350524853b447b4c79db4bad6e7254ffa0a16ae
thibaultvlm <vuillaume.thibault55@gmail.com>
2025-10-22 10:20:47 +0200

    Update all style and index in a website, i have change a logo for a website. And i have change a city.

---
+++ PHP/inscription.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Connexion</title>
+        <link rel="stylesheet" href="../CSS/connexion_inscription.css">
+    </head>
+    <body>
+        <div class="head">
+            <img src="../IMG/drapeau_de_lorraine.png" alt="Drapeau de la Lorraine" id="logoLorraine"/>
+            <div id="headLogo">
+                <img src="../IMG/logo_republique_francaise.png" alt="Logo de la république"/>
+                <img src="../IMG/logo.png" alt="Logo du site" id="logo"/>
+            </div>
+            <h2>Bienvenue à la mairie de <br/>Pont-à-Mousson !</h2>
+        </div>
+
+        <div class="form">
+            <h1>Inscription: </h1>
+            <form action="">
+                <div class="infos">
+                    <div class="divInfos">
+                        <label for="nom">Nom</label>
+                        <input type="text" name="nom" id="nom" class="textBox"/>
+                    </div>
+                    <div class="divInfos">
+                        <label for="prenom">Prénom</label>
+                        <input type="text" name="prenom" id="prenom" class="textBox"/>
+                    </div>
+                </div>
+                <label for="adresse">Adresse</label>
+                <input type="text" name="adresse" id="adresse" class="textBox"/>
+                <label for="mail">Mail</label>
+                <input type="email" name="mail" id="mail" class="textBox"/>
+                <label for="mdp">Mot de passe</label>
+                <input type="password" name="mdp" id="mdp" class="textBox"/>
+            </form>
+        </div>
+        <div class="buttonDiv">
+            <input type="button" value="Inscription" class="button"/>
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -28,8 +28,17 @@
     </header>
 
     <section id="page">
+        <video autoplay muted loop id="background-video">
+            <source src="./MEDIA/Vidéo Verdun Meuse2 - Lorraine Tourisme - FR.mp4" type="video/mp4">
+        </video>
         <div id="title">
             <h1>MIEUX ÉQUIPÉS POUR MIEUX VOUS ACCOMPAGNER</h1>
+        </div> 
+    </section>
+
+    <section id="container">
+        <div class="maps">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d19354.013677588107!2d5.371266467081616!3d49.16168313312668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sSalle%20des%20F%C3%AAtes%20%C3%A0%20proximit%C3%A9%20de%20Verdun!5e0!3m2!1sfr!2sfr!4v1761120584464!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
     <footer id="footer">
