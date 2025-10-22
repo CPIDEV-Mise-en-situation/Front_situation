5de9e0dedbfd03a8493a3fdddeae24b772981e27
SSVGLeo <troupleo@gmail.com>
2025-10-22 13:36:22 +0200

    test d'ajout des modifications du index.php et d'ajout des pages de connexion d'Elodie Restructuration des pages product et shoplist dans le dossier PHP

---
+++ PHP/inscription.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Connexion</title>
+        <link rel="stylesheet" href="../CSS/connexion-inscription.css">
+    </head>
+    <body>
+        <div class="head">
+            <img src="../IMG/drapeau_de_lorraine.png" alt="Drapeau de la Lorraine" id="logoLorraine"/>
+            <div id="headLogo">
+                <img src="../IMG/logo_republique_francaise.png" alt="Logo de la république"/>
+                <img src="../IMG/logo.png" alt="Logo du site" id="logo"/>
+            </div>
+            <h2>Bienvenue à la mairie de <br/>Verdun !</h2>
+        </div>
+
+        <div class="form">
+            <h1>Inscription: </h1>
+            <form id="inscriptionForm" action="./inscription_process" method="post">
+                <div class="infos">
+                    <div class="divInfos">
+                        <label for="nom">Nom</label>
+                        <input type="text" name="nom" id="nom" class="textBox"/>
+                    </div>
+                    <div class="divInfos">
+                        <label for="prenom">Prénom</label>
+                        <input type="text" name="prenom" id="prenom" class="textBox"/>
+                    </div>
+                </div>
+                <label for="adresse">Adresse</label>
+                <input type="text" name="adresse" id="adresse" class="textBox"/>
+                <label for="mail">Mail</label>
+                <input type="email" name="mail" id="mail" class="textBox"/>
+                <label for="mdp">Mot de passe</label>
+                <input type="password" name="mdp" id="password" class="textBox"/>
+                <div class="buttonDiv">
+                    <input type="submit" value="Inscription" class="button"/>
+                </div>
+            </form>
+        </div>
+
+        <div id="liens">
+            <a href="./connexion.php">Déjà inscrit ? Connexion</a>
+        </div>
+    </body>
+</html>
--- PHP/product.php
+++ PHP/product.php
@@ -1,5 +1,5 @@
 <?php
-require 'PHP/config.php';
+require_once __DIR__ . '/config.php';
 
 $id = (int)$_GET['id'];
 $req = $pdo->prepare("SELECT * FROM products WHERE id = ?");
@@ -16,22 +16,24 @@ if (!$product) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./CSS/index.css">
-    <link rel="stylesheet" href="./CSS/product.css">
+    <link rel="stylesheet" href="../CSS/index.css">
+    <link rel="stylesheet" href="../CSS/footer.css">
+    <link rel="stylesheet" href="../CSS/header.css">
+    <link rel="stylesheet" href="../CSS/product.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($product['title']) ?></title>
 </head>
 <body>
     <header class="header">
-        <a href="index.php">
-            <img src="./img/logo.png" alt="icon">
+        <a href="../index.php">
+            <img src="../img/logo.png" alt="icon">
         </a>
         <nav id="navigation">
             <div class="nav">
                 <ul>
                     <li><a href="shoplist.php">Produit</a></li>
                     <li><a href="">Profil</a></li>
-                    <li><a href="">Connexion</a></li>
+                    <li><a href="PHP/connexion.php">Connexion</a></li>
                 </ul>
             </div>
         </nav>
@@ -50,11 +52,31 @@ if (!$product) {
         </div>
         
         <div class="ProductImage">
-          <img src="./IMG/<?= htmlspecialchars($product['image']) ?>" alt="<?= htmlspecialchars($product['title']) ?>" class="imageProduct">
+          <img src="../IMG/<?= htmlspecialchars($product['image']) ?>" alt="<?= htmlspecialchars($product['title']) ?>" class="imageProduct">
             <p class="orderTag">Réserver</p>
         </div>
 
     </div>
+
+    <footer id="footer">
+        <img src="../IMG/logo.png" alt="Logo" id="footer-icon">
+        <div id="contact">
+            <ul>
+                <li><p>Contact :</p></li>
+                <li><a href="">19 place du Duroc</a></li>
+                <li><a href="">54700 Pont-à-Monsson</a></li>
+                <li><a href="">+33 3 83 81 10 68</a></li>
+            </ul>
+        </div>
+        <div id="infos">
+            <ul>
+                <li><a href="">Mentions légales</a></li>
+                <li><a href="">Accessibilité</a></li>
+                <li><a href="">Gestion des cookies</a></li>
+            </ul>
+        </div>
+        <div id="equipe"><p>Team CPIDEV</p></div>
+    </footer>
 
 </body>
 </html>
